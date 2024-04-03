551bc215cab14abb61e9f1b47dba5b01c0d96acc
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-04-03 19:25:08 +0700

    add nav component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent, FooterComponent],
+  imports: [RouterOutlet, LandingPageComponent, FooterComponent, NavComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'loglife-angular-frontend';
