3759c58e1268b59b6272d1bf559ef36818f72218
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-04-05 18:48:16 +0700

    refactor activity-list component to display activities using ngFor

--- src/app/activity-list/activity-list.component.ts
+++ src/app/activity-list/activity-list.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Activity } from '../activity';
+import { ActivityService } from './../activity.service';
+
 import { LayoutComponent } from '../layout/layout.component';
 import { ListedCardComponent } from '../listed-card/listed-card.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-activity-list',
   standalone: true,
-  imports: [LayoutComponent, ListedCardComponent],
+  imports: [LayoutComponent, ListedCardComponent, NgFor],
   templateUrl: './activity-list.component.html',
 })
-export class ActivityListComponent {}
+export class ActivityListComponent implements OnInit {
+  activities: Activity[] = [];
+
+  constructor(private activityService: ActivityService) {}
+
+  ngOnInit(): void {
+    this.getActivities();
+  }
+
+  getActivities(): void {
+    this.activityService.getActivities()
+      .subscribe(activities => this.activities = activities);
+  }
+}
