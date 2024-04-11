6995ca98e00329b4bc7eed82675322356a39bf57
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-04-11 21:08:39 +0700

    add activity-details component scaffold

---
+++ src/app/activity-details/activity-details.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { LayoutComponent } from '../layout/layout.component';
+
+@Component({
+  selector: 'app-activity-details',
+  standalone: true,
+  imports: [LayoutComponent],
+  templateUrl: './activity-details.component.html',
+  styleUrl: './activity-details.component.css',
+})
+export class ActivityDetailsComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ActivityListComponent } from './activity-list/activity-list.component';
+import { ActivityDetailsComponent } from './activity-details/activity-details.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'about-us', component: AboutUsComponent },
   { path: 'activities', component: ActivityListComponent },
+  { path: 'activities/:id', component: ActivityDetailsComponent}
 ];
