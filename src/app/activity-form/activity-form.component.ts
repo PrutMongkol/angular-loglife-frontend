13eb880d25558e9e8dabfc6672b133f7f493b2e9
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-04-15 19:28:45 +0700

    add form control for activity-form component

--- src/app/activity-form/activity-form.component.ts
+++ src/app/activity-form/activity-form.component.ts
@@ -1,17 +1,52 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 import { Activity } from '../activity';
 
 @Component({
   selector: 'app-activity-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './activity-form.component.html',
   styleUrl: './activity-form.component.css',
 })
 export class ActivityFormComponent {
-  formHeading = 'Create Activity';
+  activityForm = this.formBuilder.group({
+    title: [this.activity?.title || ''],
+    description: [this.activity?.description || ''],
+    type: [this.activity?.type || 'Running'],
+    date: [this.activity?.date || ''],
+    startTime: [this.activity?.startTime || ''],
+    endTime: [this.activity?.endTime || ''],
+    duration: this.formBuilder.group({
+      hours: [this.activity?.duration.hour || ''],
+      minutes: [this.activity?.duration.minute || ''],
+    }),
+    barometer: [this.activity?.barometer || '3'],
+  });
+
+  formHeading?: string = 'Create Activity';
+  submitButtonLabel?: string = 'Create';
+
   activity?: Activity;
+
+  activityTypeIcons: {[key: string]: string}[] = [
+    { type: 'Running', icon: 'sprint' },
+    { type: 'Cycling', icon: 'directions_bike' },
+    { type: 'Swimming', icon: 'pool' },
+    { type: 'Walking', icon: 'directions_walk' },
+    { type: 'Hiking', icon: 'hiking' },
+    { type: 'Other', icon: 'timer' },
+  ];
+
+  barometerIcons: {[key: string]: string}[] = [
+    { value: '1', icon: 'sentiment_very_dissatisfied', label: 'Very Weak' },
+    { value: '2', icon: 'sentiment_dissatisfied', label: 'Weak' },
+    { value: '3', icon: 'sentiment_neutral', label: 'Normal' },
+    { value: '4', icon: 'sentiment_satisfied', label: 'Strong' },
+    { value: '5', icon: 'sentiment_very_satisfied', label: 'Very Strong' },
+  ];
 
   barometerColor: { [key: string]: string } = {
     1: 'bg-info',
@@ -22,11 +57,12 @@ export class ActivityFormComponent {
   };
   barometerColorClass: string;
 
-  constructor() {
-    this.barometerColorClass = this.barometerColor[this.activity?.barometer || "1"];
+  constructor(private formBuilder: FormBuilder) {
+    this.barometerColorClass =
+      this.barometerColor[this.activity?.barometer || '1'];
   }
 
   handleSubmit(): void {
-    alert("Not Implemented Yet")
+    alert('Not Implemented Yet');
   }
 }
