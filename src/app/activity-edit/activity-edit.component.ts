e8985febf15182b36a4f65808c07a80d4e189cfb
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-04-16 23:26:24 +0700

    update activity-edit to use activity-form component

--- src/app/activity-edit/activity-edit.component.ts
+++ src/app/activity-edit/activity-edit.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { LayoutComponent } from '../layout/layout.component';
+import { ActivatedRoute } from '@angular/router';
+
+import { ActivityFormComponent } from '../activity-form/activity-form.component';
 
 @Component({
   selector: 'app-activity-edit',
   standalone: true,
-  imports: [],
+  imports: [LayoutComponent, ActivityFormComponent],
   templateUrl: './activity-edit.component.html',
 })
 export class ActivityEditComponent {
+  activityId = this.route.snapshot.paramMap.get('id') || undefined;
 
+  constructor(
+    private route: ActivatedRoute
+  ) {}
 }
