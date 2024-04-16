2241093570e765edf8e4bfa2b66d3bc8ccb8f11b
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-04-16 22:42:42 +0700

    add blank activity-edit component

---
+++ src/app/activity-edit/activity-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './activity-edit.component.html',
+})
+export class ActivityEditComponent {
+
+}
